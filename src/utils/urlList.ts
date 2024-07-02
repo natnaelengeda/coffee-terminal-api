431cf649c2c5b73e75ae0229565972646f429cee
Natnael Engeda <nattynengeda@gmail.com>
2024-07-03 00:50:00 +0300

    Update dev url

--- src/utils/urlList.ts
+++ src/utils/urlList.ts
@@ -2,6 +2,6 @@ export const urlList = [
   'http://localhost:3000',
   'http://localhost:5173',
   "https://www.coffeeterminalet.com",
-  "http://coffee-terminal-dev.vercel.app",
+  "https://coffee-terminal-dev.vercel.app",
   "https://coffee-terminal-admin.vercel.app",
 ]
